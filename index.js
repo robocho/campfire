a44292931e941db89359f9de01711a2eba6be3bb
abrognara <abrognara97@gmail.com>
2019-04-26 11:51:15 -0400

    Add files via upload
    sandros first commit????

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var _ = require("underscore");
 var Channel = require('./models/channels');
 const port = 3000;
 
+//setting up express & handlebars
+
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -29,27 +31,71 @@ mongoose.connection.on('error', function() {
 })
 
 
-
 app.get('/',function(req,res){
-	var new_channel = new Channel({name: "my channel", genre: "pop",date_created: Date.now(), queue: ["www.youtube.com/dasd", "www.youtube.com/dasd"]});
-	new_channel.save(function(err){
-		if(err) {
-			console.log("ERROR")
-			
+	var ch = []
+
+	// Callback function to wait for channel data from DB before rendering page
+	function getChannelData(callback) {
+		// Channel.find({},{"name":1, "_id":0}, function(err, channels) {
+		Channel.find({}, function(err, channels) {
+			if (err) { throw err };
+			ch = channels;
+			callback();
+		});
+	}
+
+	app.post('/', function(req, res){
+		var body = req.body;
+		var videoURL = req.body.song;
+		
+		// check if the videoURL exists, then extract the video paramter and construct an embeded video link
+		// set autoplay=1 parameter to play video on page load
+		if (videoURL != '') {
+			var param = videoURL.split('v=')[1];
+			videoURL = "https://www.youtube.com/embed/" + param + "?autoplay=1";
 		}
-		console.log("wowHO")
-	})
-	//collection.insertOne(new_channel)
-	//console.log(Channel.findOne());
-	res.render('home');
+
+		var new_channel = new Channel({
+			name: body.name,
+			genre: body.genre,
+			date_created: Date.now(),
+			queue: [videoURL],
+			current_song: ''
+		});
+		new_channel.save(function err(){
+			if(err) { console.log("ERROR") }
+		});
+		res.redirect('/show');
+	});
+	
+	getChannelData(function(){
+		res.render('home', {channels: ch});
+	});
 });
 
-app.get('/:channel_name', function(req, res){
+app.get('/channel/:name', function(req,res){
+	var ch = {};
+	// Callback function to wait for channel data from DB before rendering page
+	// Match :name parameter with a channel name, save that channel, and render the page with its data
+	function getChannelData(callback) {
+		Channel.find({name: req.params.name}, function(err, channel){
+			if (err) { throw err; }
+			ch = channel;
+			callback();
+		});
+	}
+	getChannelData(function(){
+		res.render('channel-page', {channel: ch, current_song: ch[0].queue[0]});
+	});
+});
 
-	
+app.get('/show', function(req, res){
+	Channel.find({}, function(err, channels) {
+		if (err) { throw err };
+		res.send(channels);
+	});
 }) 
 app.listen(port, function() {
 	console.log("listening on port 3000!");
 
-	
 });
