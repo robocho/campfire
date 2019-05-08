1062b1d7cc4722376b6b8133eee0f2829c505b72
Robert Choe <Robert.d.choe67@gmail.com>
2019-05-08 18:26:33 -0400

    update

--- models/channels.js
+++ models/channels.js
@@ -6,6 +6,12 @@ var SongSchema = new Schema(
     mp3_link: {type: String, required: true},
   }
 );
+var CommentSchema = new Schema ({
+  name: {type: String, required: true, max: 100},
+  date_created: {type: Date},
+  comment: {type: String, required: true, max: 400},
+})
+
 
 var ChannelSchema = new Schema(
   {
@@ -18,11 +24,6 @@ var ChannelSchema = new Schema(
   }
 );
 
-var CommentSchema = new Schema ({
-  name: {type: String, required: true, max: 100},
-  date_created: {type: Date},
-  comment: {type: String, required: true, max: 400},
-})
 
 //Export model - compiles a model
 // instances of models are called documents
