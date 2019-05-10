32d979a478ed539456ee85d312c02080581e69f5
Robert Choe <Robert.d.choe67@gmail.com>
2019-05-10 02:00:32 -0400

    search

--- public/js/home.js
+++ public/js/home.js
@@ -5,3 +5,35 @@ $(document).ready(function(){
 	socket.emit('home page loaded');
 	
 });
+/*
+$('#search-element').on('input', function(e){
+  var input = $(e.target).val();
+  if (input === '') {
+    $('.entry').show();
+  } else {
+    $('.entry').hide();
+      $('.entry').each(function(){
+        var channelName = $(this).attr('id');
+        var re = new RegExp("^"+input,"g");
+        if (channelName.match(re)) {
+            $(this).show();
+        }
+    });
+  }
+});
+*/
+$("#search-element").on("change paste keyup select", function(e){
+	var input = $(e.target).val();
+	if (input === '') {
+    $('.entry').show();
+  } else {
+    $('.entry').hide();
+      $('.entry').each(function(){
+        var channelName = $(this).attr('id');
+        var re = new RegExp("^"+input,"g");
+        if (channelName.match(re)) {
+            $(this).show();
+        }
+    });
+  }
+});
